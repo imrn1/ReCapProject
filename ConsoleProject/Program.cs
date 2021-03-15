c6c5a8c672fe8e704032f8885c8529c7555b3764
İmran Aykut <49922016+imrn1@users.noreply.github.com>
2021-03-15 22:41:37 +0300

    Update Program.cs

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -12,8 +12,8 @@ namespace ConsoleProject
         {
             CarManager carManager = new CarManager(new InMemory());
 
-            List<Car> cars = carManager.GetAll();
             Console.WriteLine("---------------- GetAll cars ---------------------\n");
+            List<Car> cars = carManager.GetAll();
             showCarList(cars);
 
             Console.WriteLine("\n---------------- Add car ---------------------\n");
